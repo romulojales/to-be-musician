1bb9d2b7ba665428b4872b03c1ca0db6688aa066
Romulo Jales <romulo.jales@corp.globo.com>
2013-09-28 16:47:20 -0300

    removing url

--- to_be_a_musician/djtinysong/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-from djtinysong import SONG_URL
-
-
-urlpatterns = patterns('djtinysong',
-    url(r'^search/(?P<params>(.+))$', 'views.search', name='tinysong_search'),
-)
