ae43fd4c5479767fbb5c4d21f23f7bc1a7182d2c
Romulo Jales <romulo.jales@corp.globo.com>
2013-09-28 18:29:27 -0300

    tests

--- to_be_a_musician/djtinysong/tests/__init__.py
+++ to_be_a_musician/djtinysong/tests/__init__.py
@@ -1,2 +1,16 @@
+
 from .test_utils import *
 from .test_filter_song_flash_player import *
+
+
+class RequestJson(object):
+    code = 200
+
+    def __init__(self, json):
+        self.content = json
+
+    def get(self, *args, **kwargs):
+        return self.content
+
+    def json(self):
+        return self.content
--- to_be_a_musician/djtinysong/tests/test_utils.py
+++ to_be_a_musician/djtinysong/tests/test_utils.py
@@ -1,10 +1,13 @@
 
 import unittest
-from djtinysong.utils import search_music
+
+from djtinysong.utils import get, search_music
 import mock
+from __init__ import RequestJson
 
 
-class TestUtils(unittest.TestCase):
+class TestSearchUtils(unittest.TestCase):
+
     def test_simple_search(self):
         with  mock.patch("djtinysong.utils.get") as mck:
             mck.return_value = [{
@@ -18,3 +21,50 @@ class TestUtils(unittest.TestCase):
         }]
             musics = search_music("arg")
             self.assertEquals(len(musics), 1)
+
+    def test_get_with_2_objects(self):
+        with  mock.patch("djtinysong.utils.get") as mck:
+            mck.return_value = [{
+        "Url": "http:\/\/tinysong.com\/8We2",
+        "SongID": 269743,
+        "SongName": "The Legend Of Lil' Beethoven",
+        "ArtistID": 7620,
+        "ArtistName": "Sparks",
+        "AlbumID": 204019,
+        "AlbumName": "Sparks"
+        }, {
+        "Url": "http:\/\/tinysong.com\/abc",
+        "SongID": 2697431,
+        "SongName": "The Legend Of Lila' Beethoven",
+        "ArtistID": 76201,
+        "ArtistName": "Sparkss",
+        "AlbumID": 2040191,
+        "AlbumName": "Sparkfs"
+        }]
+            musics = search_music("abc")
+            self.assertEquals(len(musics), 2)
+
+
+class TestGetUtils(unittest.TestCase):
+
+    def test_2_objects_in_response(self):
+        with  mock.patch("requests.get") as mck:
+            mck.return_value = RequestJson([{
+        "Url": "http:\/\/tinysong.com\/8We2",
+        "SongID": 269743,
+        "SongName": "The Legend Of Lil' Beethoven",
+        "ArtistID": 7620,
+        "ArtistName": "Sparks",
+        "AlbumID": 204019,
+        "AlbumName": "Sparks"
+        }, {
+        "Url": "http:\/\/tinysong.com\/abc",
+        "SongID": 2697431,
+        "SongName": "The Legend Of Lila' Beethoven",
+        "ArtistID": 76201,
+        "ArtistName": "Sparkss",
+        "AlbumID": 2040191,
+        "AlbumName": "Sparkfs"
+        }])
+            musics = get("abc", {})
+            self.assertEquals(len(musics), 2)
