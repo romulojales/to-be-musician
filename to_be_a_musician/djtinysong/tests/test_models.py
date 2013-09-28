2f05f357a828180c911f4267d4374d1fdf323127
Romulo Jales <romulo.jales@corp.globo.com>
2013-09-27 23:13:08 -0300

    song class

---
+++ to_be_a_musician/djtinysong/tests/test_models.py
@@ -0,0 +1,25 @@
+import unittest
+from djtinysong.models import Song
+
+
+class Test(unittest.TestCase):
+
+    def test_simple_class_instance(self):
+        dictionary = {
+        "Url": "http:\/\/tinysong.com\/8We2",
+        "SongID": 269743,
+        "SongName": "The Legend Of Lil' Beethoven",
+        "ArtistID": 7620,
+        "ArtistName": "Sparks",
+        "AlbumID": 204019,
+        "AlbumName": "Sparks"
+        }
+
+        music = Song(**dictionary)
+        self.assertEquals(music.tinySongURL, dictionary["Url"])
+        self.assertEquals(music.songId, dictionary["SongID"])
+        self.assertEquals(music.songName, dictionary["SongName"])
+        self.assertEquals(music.artistId, dictionary["ArtistID"])
+        self.assertEquals(music.artistName, dictionary["ArtistName"])
+        self.assertEquals(music.albumId, dictionary["AlbumID"])
+        self.assertEquals(music.albumName, dictionary["AlbumName"])
