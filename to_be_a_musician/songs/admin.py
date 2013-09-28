6c98c24d2d6482ce97ffbea0dd05325e19d7f1bc
Klaus Laube <kplaube@gmail.com>
2013-09-28 16:00:51 -0300

    Created a basic song_detail page and added admin entries

---
+++ to_be_a_musician/songs/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from songs.models import Artist, Album, Song
+
+
+class SongsBaseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'api_id')
+
+
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('name', 'artist', 'album', )
+
+
+admin.site.register(Artist, SongsBaseAdmin)
+admin.site.register(Album, SongsBaseAdmin)
+admin.site.register(Song, SongAdmin)
