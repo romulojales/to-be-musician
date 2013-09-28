5e0b4f558e685ce14c911da9f2e876448bafe876
Romulo Jales <romulo.jales@corp.globo.com>
2013-09-27 22:05:20 -0300

    tinysong

--- to_be_a_musician/settings.py
+++ to_be_a_musician/settings.py
@@ -128,6 +128,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'djtinysong'
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
@@ -160,3 +161,6 @@ LOGGING = {
         },
     }
 }
+
+TINYSONG_KEY = "1dbea9a0d261b0e1c2c738bd456dd751"
+
