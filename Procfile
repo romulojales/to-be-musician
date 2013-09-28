5e36350d79ee1e4da5feab8180c8099770d15d55
Klaus Laube <kplaube@gmail.com>
2013-09-27 21:57:16 -0300

    Prepared to deploy to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn to_be_a_musician.wsgi
--- to_be_a_musician/settings.py
+++ to_be_a_musician/settings.py
@@ -1,4 +1,4 @@
-# Django settings for to_be_a_musician project.
+#/ Django settings for to_be_a_musician project.
 
 from unipath import Path
 
@@ -26,7 +26,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
