0a75f02531b67e9d4765af3b78835316fe7969d1
Klaus Laube <kplaube@gmail.com>
2013-09-28 15:38:55 -0300

    Created song_detail view

--- to_be_a_musician/songs/tests/test_views.py
+++ to_be_a_musician/songs/tests/test_views.py
@@ -1,16 +1,34 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
-from songs import models
+from model_mommy import mommy
+from songs import models, views
 
 
 class SongsSongViewTestCase(TestCase):
 
     @classmethod
     def setUpClass(cls):
+        cls.artist = mommy.make('songs.artist', name='Metallica')
+        cls.album = mommy.make('songs.album', name='Master of Puppets')
+        cls.song = mommy.make('songs.song', name='Master of Puppets',
+                              artist=cls.artist, album=cls.album)
+
         cls.url = reverse('songs_song', kwargs={
             'artist_slug': 'metallica',
             'song_slug': 'master-of-puppets',
         })
 
+    @classmethod
+    def tearDownClass(cls):
+        cls.artist.delete()
+        cls.album.delete()
+        cls.song.delete()
+
+    def setUp(self):
+        self.response = self.client.get(self.url)
+
     def test_song_route(self):
         self.assertEqual(self.url, '/songs/metallica/master-of-puppets/')
+
+    def test_is_available_when_have_band_and_song(self):
+        self.assertEqual(self.response.status_code, 200)
--- to_be_a_musician/songs/views.py
+++ to_be_a_musician/songs/views.py
@@ -1,6 +1,15 @@
-from django.views.generic.base import TemplateView
+from django.views.generic import DetailView
 from songs.models import Song
 
 
-class SongView(TemplateView):
-    template_name = 'common/index.html'
+class SongView(DetailView):
+    model = Song
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+
+        artist_slug = self.kwargs['artist_slug']
+        song_slug = self.kwargs['song_slug']
+
+        return queryset.get(slug=song_slug, artist__slug=artist_slug)
