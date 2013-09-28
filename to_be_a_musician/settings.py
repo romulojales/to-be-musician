06e04effe4fe6fc03a8a6cb731919f6109b36d5b
Klaus Laube <kplaube@gmail.com>
2013-09-27 21:33:57 -0300

    Created index.html to make Kenji's life easier

--- to_be_a_musician/settings.py
+++ to_be_a_musician/settings.py
@@ -124,10 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
--- to_be_a_musician/urls.py
+++ to_be_a_musician/urls.py
@@ -1,17 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'to_be_a_musician.views.home', name='home'),
-    # url(r'^to_be_a_musician/', include('to_be_a_musician.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+    url(r'^admin/', include(admin.site.urls)),
 )
