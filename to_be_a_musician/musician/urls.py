9d380086b37e83915b70e513f32f9f17afac8ed0
Klaus Laube <kplaube@gmail.com>
2013-09-29 15:52:59 -0300

    Fixed #15 -- Added login_required decorator to the view

--- to_be_a_musician/musician/urls.py
+++ to_be_a_musician/musician/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth.decorators import login_required
 
 from musician.views import SongStateView, MusicianView
 
+song_state_view = login_required(SongStateView.as_view())
 
 urlpatterns = patterns('',
     url(r'(?P<state>(learn|learning|learned))/(?P<id>\d+)/$',
-        SongStateView.as_view(), name='musician_song_state'),
+        song_state_view, name='musician_song_state'),
     url(r'user/(?P<user_name>(.*))/$', MusicianView.as_view(),
         name='musician_page'),
 )
