75bfecfc24aeac24f7c289abf883f44da6908ee0
Kenji Yamamoto <spam.kenji@gmail.com>
2013-09-28 22:17:25 -0300

    Inclusao do Visual inicial do projeto

---
+++ to_be_a_musician/common/config.rb
@@ -0,0 +1,11 @@
+css_dir = "static/css"
+sass_dir = "static/scss"
+images_dir = "static/img"
+javascripts_dir = "static/js"
+http_path = "/"
+relative_assets = true
+
+# Specify the output style/environment
+# output_style = :expanded
+output_style = :compressed
+environment = :production
