6013c5e9e92eb2357e28503646241ff8668fd0b9
Klaus Laube <kplaube@gmail.com>
2013-09-29 02:40:35 -0300

    Fixed #13 -- Added login_required decorator to CRUD operations

--- to_be_a_musician/songs/urls.py
+++ to_be_a_musician/songs/urls.py
@@ -1,20 +1,25 @@
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
 from songs.views import (InterpretationView, InterpretationCreateView,
                          InterpretationDeleteView, InterpretationUpdateView,
                          SongView)
 
 SONG_URL_PATTERN = '(?P<artist_slug>[\w_-]+)/(?P<song_slug>[\w_-]+)'
 
+interpretation_create_view = login_required(InterpretationCreateView.as_view())
+interpretation_update_view = login_required(InterpretationUpdateView.as_view())
+interpretation_delete_view = login_required(InterpretationDeleteView.as_view())
+
 urlpatterns = patterns('',
     url(r"{0}/interpretation/add/$".format(SONG_URL_PATTERN),
-        InterpretationCreateView.as_view(), name='songs_interpretation_add'),
+        interpretation_create_view, name='songs_interpretation_add'),
     url(r"{0}/interpretation/(?P<id>\d+)/$".format(SONG_URL_PATTERN),
         InterpretationView.as_view(), name='songs_interpretation_detail'),
     url(r"{0}/interpretation/(?P<id>\d+)/edit/$".format(SONG_URL_PATTERN),
-        InterpretationUpdateView.as_view(), name='songs_interpretation_edit'),
+        interpretation_update_view, name='songs_interpretation_edit'),
     url(r"{0}/interpretation/(?P<id>\d+)/delete/$".format(SONG_URL_PATTERN),
-        InterpretationDeleteView.as_view(), name='songs_interpretation_delete'),
+        interpretation_delete_view, name='songs_interpretation_delete'),
 
     url(r"{0}/$".format(SONG_URL_PATTERN), SongView.as_view(), name='songs_song'),
 )
